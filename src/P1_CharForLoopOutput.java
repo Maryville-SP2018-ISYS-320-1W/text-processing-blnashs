37a03c862350a51e8847f53b0d3bc5505f537fc1
Barbara Nash <bnash1@live.maryville.edu>
2018-04-08 12:57:12 -0500

    string

--- src/P1_CharForLoopOutput.java
+++ src/P1_CharForLoopOutput.java
@@ -7,22 +7,22 @@
 /*
  Your expected results after the input is provided:
  
- 1.
- 2.
- 3. 
- 4.
+ 1.  e <= g++
+ 2.  n <= s++
+ 3.  z <= a++
+ 4.  q <= r++
   
  */
 public class P1_CharForLoopOutput {
 
 	public static void main(String[] args) {
-		// put your call to printRange below here
+		//printRange('e', 'g');
 
 	}
 	
 	public static void printRange(char startLetter, char endLetter ) {
 		for( char letter = startLetter; letter <= endLetter; letter++ ) {
-			System.out.print( letter );
+			System.out.print(e, g);
 		}
 		
 		System.out.println();
